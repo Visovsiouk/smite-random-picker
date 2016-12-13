500b7596e712d9c10d23965a2a052a3904b5297e
Visovsiouk <visovsiouk@gmail.com>
2016-12-13 10:37:51 +0200

    Adjusted wheel for even segments

--- src/GodWheel.js
+++ src/GodWheel.js
@@ -14,7 +14,7 @@ class GodWheel extends React.Component  {
            return god;
 	    }); 
         var segments = gods.length;
-        console.log(gods);
+        var text = "12px Trebuchet";
         let canvas = ReactDOM.findDOMNode(this.refs.canvas),
         context = canvas.getContext("2d");
         canvas.style.width ='100%';
@@ -30,52 +30,66 @@ class GodWheel extends React.Component  {
 
         // how thick you want a segment
         var segmentDepth = canvas.width / 2;
+        
+
+        if ( segments >= 80 ) {
+            text = "12px Arial";
+        } else if (segments < 80 && segments >= 60) {
+            text = "14px Arial";
+        } else {
+            text = "20px Arial";
+        }
 
         function init() {
             drawSegments(segmentDepth);
         }
 
         function drawSegments(radius) {
-            var rotation = 360 / segments;
-            for (var i = 0; i < segments; i++) {
-                context.beginPath();
-                context.moveTo(x, y);
-                context.arc(x, y, radius, i*pieAngle, (i+1)*pieAngle, false);
-                context.lineWidth = segmentDepth;
-                var hueValue = i * 15;
-                context.fillStyle = 'hsl(' + hueValue + ',70%, 60%)';
-                context.fill();
-            }
-            for (var j = 0; j < segments; j++) {
-                var rotated = false;
-                if (segments % 2 == 0) {
-                    if (rotated === false) {
-                        rotated = true;
-                        console.log('test')
-                    }
+            if (segments > 1) {
+                var rotation = 360 / segments;
+                for (var i = 0; i < segments; i++) {
                     context.beginPath();
                     context.moveTo(x, y);
-                    context.fillStyle = '#000000';
-                    context.font="20px Georgia";
-                    context.fillText(gods[j], x - outerRadius + margin, y);
+                    context.arc(x, y, radius, i*pieAngle, (i+1)*pieAngle, false);
+                    context.lineWidth = segmentDepth;
+                    var hueValue = i * 15;
+                    context.fillStyle = 'hsl(' + hueValue + ',70%, 60%)';
                     context.fill();
-                    context.save();
-                    context.translate(x, y);
-                    context.rotate(rotation * Math.PI / 180);
-                    context.translate(-x, -y);    
-                } else {
-                    context.beginPath();
-                    context.moveTo(x, y);
-                    context.fillStyle = '#000000';
-                    context.font="20px Georgia";
-                    context.fillText(gods[j], x - outerRadius + margin, y);
-                    context.fill();
-                    context.save();
-                    context.translate(x, y);
-                    context.rotate(rotation * Math.PI / 180);
-                    context.translate(-x, -y);
                 }
-            }
+                var flag = false;
+                for (var j = 0; j < segments; j++) {
+                    if (segments % 2 === 0) {
+                        if (flag === false) {
+                            context.save();
+                            context.translate(x, y);
+                            context.rotate((rotation / 2 ) * Math.PI / 180);
+                            context.translate(-x, -y);    
+                            flag = true;
+                        }
+                        context.beginPath();
+                        context.moveTo(x, y);
+                        context.fillStyle = '#000000';
+                        context.font = text;
+                        context.fillText(gods[j], x - outerRadius + margin, y);
+                        context.fill();
+                        context.save();
+                        context.translate(x, y);
+                        context.rotate(rotation * Math.PI / 180);
+                        context.translate(-x, -y);    
+                    } else {
+                        context.beginPath();
+                        context.moveTo(x, y);
+                        context.fillStyle = '#000000';
+                        context.font = text;
+                        context.fillText(gods[j], x - outerRadius + margin, y);
+                        context.fill();
+                        context.save();
+                        context.translate(x, y);
+                        context.rotate(rotation * Math.PI / 180);
+                        context.translate(-x, -y);
+                    }
+                }
+            }   
         }
 
         // start drawing our chart
